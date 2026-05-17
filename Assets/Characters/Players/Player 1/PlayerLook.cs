84587900de99cad78aad280db1077555d29e6841
Zainab <202302211@student.polytechnic.bh>
2026-05-17 23:24:45 +0300

    fixed gun clipping issue, and u can now shoot a zombie ;-;

---
+++ Assets/Characters/Players/Player 1/PlayerLook.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Transform cameraPivot;
+    public float mouseSensitivity = 150f;
+
+    float xRotation = 0f;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        cameraPivot.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
